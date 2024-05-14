448b42aa07dc7f68e26c1e44d7f172ba7473d432
guempelp0 - Uni PC <patrikguempel@gmail.com>
2024-05-14 18:40:08 +0200

    added path management

--- own_attempt.py
+++ own_attempt.py
@@ -15,7 +15,7 @@ mli_min = xr.open_dataset('./norm/input_min.nc')
 mli_max = xr.open_dataset('./norm/input_max.nc')
 mlo_scale = xr.open_dataset('./norm/output_scale.nc')
 
-def load_nc_dir_with_generator(filelist: list):
+def loadNCdir(filelist: list):
     def gen():
         for file in filelist:
             # read mli
@@ -56,40 +56,44 @@ def main():
     vars_mlo = ['ptend_t', 'ptend_q0001', 'cam_out_NETSW', 'cam_out_FLWDS', 'cam_out_PRECSC',
                 'cam_out_PRECC', 'cam_out_SOLS', 'cam_out_SOLL', 'cam_out_SOLSD', 'cam_out_SOLLD']
 
-    # normalization/scaling factors
-    #mli_mean = xr.open_dataset('../../norm_factors/mli_mean.nc', engine='netcdf4')
-    #mli_min = xr.open_dataset('../../norm_factors/mli_min.nc', engine='netcdf4')
-    #mli_max = xr.open_dataset('../../norm_factors/mli_max.nc', engine='netcdf4')
-    #mlo_scale = xr.open_dataset('../../norm_factors/mlo_scale.nc', engine='netcdf4')
-
     # train dataset for HPO
     # (subsampling id done here by "stride_sample")
-    stride_sample = 37  # about ~20% assuming we will use 1/7 subsampled dataset for full training.
-    f_mli1 = glob.glob('/pscratch/sd/s/sungduk/hugging/E3SM-MMF_ne4/train/*/E3SM-MMF.mli.000[1234567]-*-*-*.nc')
-    f_mli2 = glob.glob('/pscratch/sd/s/sungduk/hugging/E3SM-MMF_ne4/train/*/E3SM-MMF.mli.0008-01-*-*.nc')
-    f_mli = sorted([*f_mli1, *f_mli2])
-    random.shuffle(f_mli)  # to reduce IO bottleneck
-    f_mli = f_mli[::stride_sample]
+    f_mli, f_mli_val = getDataPaths(remote = True)
+    model = None #TODO
+    train(f_mli, f_mli_val, model)
 
-    # validation dataset for HPO
-    f_mli1 = glob.glob('/pscratch/sd/s/sungduk/hugging/E3SM-MMF_ne4/train/*/E3SM-MMF.mli.0008-0[23456789]-*-*.nc')
-    f_mli2 = glob.glob('/pscratch/sd/s/sungduk/hugging/E3SM-MMF_ne4/train/*/E3SM-MMF.mli.0008-1[012]-*-*.nc')
-    f_mli3 = glob.glob('/pscratch/sd/s/sungduk/hugging/E3SM-MMF_ne4/train/*/E3SM-MMF.mli.0009-01-*-*.nc')
-    f_mli_val = sorted([*f_mli1, *f_mli2, *f_mli3])
-    random.shuffle(f_mli_val)
-    f_mli_val = f_mli_val[::stride_sample]
+def getDataPaths(remote: bool, stride_sample: int = 37):
+    if (remote):
+        f_mli1 = glob.glob('../climsim-dataset/ClimSim_low-res/train/*/E3SM-MMF.mli.000[1234567]-*-*-*.nc')
+        f_mli2 = glob.glob('../climsim-dataset/ClimSim_low-res/train/*/E3SM-MMF.mli.0008-01-*-*.nc')
+        f_mli = sorted([*f_mli1, *f_mli2])
+        random.shuffle(f_mli)  # to reduce IO bottleneck
+        f_mli = f_mli[::stride_sample]
 
-def train(N_EPOCHS: int = 30, shuffle_buffer: int = 12 * 384, batch_size = 96): #ncol = 384      384/4 = 96
+        # validation dataset for HPO
+        f_mli1 = glob.glob('../climsim-dataset/ClimSim_low-res/train/*/E3SM-MMF.mli.0008-0[23456789]-*-*.nc')
+        f_mli2 = glob.glob('../climsim-dataset/ClimSim_low-res/train/*/E3SM-MMF.mli.0008-1[012]-*-*.nc')
+        f_mli3 = glob.glob('../climsim-dataset/ClimSim_low-res/train/*/E3SM-MMF.mli.0009-01-*-*.nc')
+        f_mli_val = sorted([*f_mli1, *f_mli2, *f_mli3])
+        random.shuffle(f_mli_val)
+        f_mli_val = f_mli_val[::stride_sample]
+    else:
+        f_mli = "./sampledata/sample.nc"
+        f_mli_val = "./sampledata/sample2.nc"
+
+    return f_mli, f_mli_val
+
+def train(f_mli, f_mli_val, model, N_EPOCHS: int = 30, shuffle_buffer: int = 12 * 384, batch_size = 96): #ncol = 384      384/4 = 96
     for n in range(N_EPOCHS):
         random.shuffle(f_mli)
-        tds = load_nc_dir_with_generator(f_mli)  # global shuffle by file names
+        tds = loadNCdir(f_mli)  # global shuffle by file names
         tds = tds.unbatch()
         # local shuffle by elements    tds = tds.shuffle(buffer_size=shuffle_buffer, reshuffle_each_iteration=False)
         tds = tds.batch(batch_size)
         tds = tds.prefetch(buffer_size=int(shuffle_buffer / 384))  # in realtion to the batch size
 
         random.shuffle(f_mli_val)
-        tds_val = load_nc_dir_with_generator(f_mli_val)
+        tds_val = loadNCdir(f_mli_val)
         tds_val = tds_val.unbatch()
         tds_val = tds_val.shuffle(buffer_size=shuffle_buffer, reshuffle_each_iteration=False)
         tds_val = tds_val.batch(batch_size)
