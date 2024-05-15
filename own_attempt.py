e2dc12d6aa77aae87d58ac62b1221593888deea8
patrik.unipc <patrikguempel@gmail.com>
2024-05-15 16:55:12 +0200

    modified qrun.py fixed import error

--- own_attempt.py
+++ own_attempt.py
@@ -1,19 +1,27 @@
 import xarray as xr
 import numpy as np
 import tensorflow as tf
-from tensorflow.python import keras
+from tensorflow import keras
 from keras import models
 from keras import layers
 from keras import callbacks
+from tensorflow.keras.optimizers import Adam
 import os
 import argparse
 import glob
 import random
+import sys
 
 mli_mean = xr.open_dataset('./norm/input_mean.nc')
 mli_min = xr.open_dataset('./norm/input_min.nc')
 mli_max = xr.open_dataset('./norm/input_max.nc')
 mlo_scale = xr.open_dataset('./norm/output_scale.nc')
+
+# in/out variable lists
+vars_mli = ['state_t', 'state_q0001', 'state_ps', 'pbuf_SOLIN', 'pbuf_LHFLX', 'pbuf_SHFLX']
+vars_mlo = ['ptend_t', 'ptend_q0001', 'cam_out_NETSW', 'cam_out_FLWDS', 'cam_out_PRECSC',
+            'cam_out_PRECC', 'cam_out_SOLS', 'cam_out_SOLL', 'cam_out_SOLSD', 'cam_out_SOLLD']
+
 
 def loadNCdir(filelist: list):
     def gen():
@@ -50,20 +58,47 @@ def loadNCdir(filelist: list):
         output_shapes=((None, 124), (None, 128))
     )
 
-def main():
-    # in/out variable lists
-    vars_mli = ['state_t', 'state_q0001', 'state_ps', 'pbuf_SOLIN', 'pbuf_LHFLX', 'pbuf_SHFLX']
-    vars_mlo = ['ptend_t', 'ptend_q0001', 'cam_out_NETSW', 'cam_out_FLWDS', 'cam_out_PRECSC',
-                'cam_out_PRECC', 'cam_out_SOLS', 'cam_out_SOLL', 'cam_out_SOLSD', 'cam_out_SOLLD']
 
+def main():
     # train dataset for HPO
     # (subsampling id done here by "stride_sample")
-    f_mli, f_mli_val = getDataPaths(remote = True)
-    model = None #TODO
+
+    remote = sys.argv[1] == "remote"
+
+    f_mli, f_mli_val = getDataPaths(remote=False)
+    model: keras.Model = createModel()
     train(f_mli, f_mli_val, model)
 
+
+def createModel():
+    input_length = 2 * 60 + 4
+    output_length_lin = 2 * 60
+    output_length_relu = 8
+    output_length = output_length_lin + output_length_relu
+    n_nodes = 512
+
+    # construct a model
+    input_layer = keras.layers.Input(shape=(input_length,), name='input')
+    hidden_0 = keras.layers.Dense(n_nodes, activation='relu')(input_layer)
+    hidden_1 = keras.layers.Dense(n_nodes, activation='relu')(hidden_0)
+    output_pre = keras.layers.Dense(output_length, activation='elu')(hidden_1)
+    output_lin = keras.layers.Dense(output_length_lin, activation='linear')(output_pre)
+    output_relu = keras.layers.Dense(output_length_relu, activation='relu')(output_pre)
+    output_layer = keras.layers.Concatenate()([output_lin, output_relu])
+
+    model = keras.Model(input_layer, output_layer, name='Emulator')
+    model.summary()
+
+    # compile
+    model.compile(optimizer=keras.optimizers.Adam(),  # optimizer=keras.optimizers.Adam(learning_rate=clr),
+                  loss='mse',
+                  metrics=['mse', 'mae', 'accuracy'])
+
+    return model
+
+
 def getDataPaths(remote: bool, stride_sample: int = 37):
-    if (remote):
+    if remote:
         f_mli1 = glob.glob('../climsim-dataset/ClimSim_low-res/train/*/E3SM-MMF.mli.000[1234567]-*-*-*.nc')
         f_mli2 = glob.glob('../climsim-dataset/ClimSim_low-res/train/*/E3SM-MMF.mli.0008-01-*-*.nc')
         f_mli = sorted([*f_mli1, *f_mli2])
@@ -78,13 +113,14 @@ def getDataPaths(remote: bool, stride_sample: int = 37):
         random.shuffle(f_mli_val)
         f_mli_val = f_mli_val[::stride_sample]
     else:
-        f_mli = "./sampledata/sample.nc"
-        f_mli_val = "./sampledata/sample2.nc"
+        f_mli = ["./sampledata/sample.nc"]
+        f_mli_val = ["./sampledata/sample2.nc"]
 
     return f_mli, f_mli_val
 
-def train(f_mli, f_mli_val, model, N_EPOCHS: int = 30, shuffle_buffer: int = 12 * 384, batch_size = 96): #ncol = 384      384/4 = 96
-    for n in range(N_EPOCHS):
+
+def train(f_mli, f_mli_val, model: keras.Model, n_epochs: int = 30, shuffle_buffer: int = 12 * 384, batch_size=96):  # ncol = 384      384/4 = 96
+    for n in range(n_epochs):
         random.shuffle(f_mli)
         tds = loadNCdir(f_mli)  # global shuffle by file names
         tds = tds.unbatch()
@@ -102,4 +138,7 @@ def train(f_mli, f_mli_val, model, N_EPOCHS: int = 30, shuffle_buffer: int = 12 
         print(f'Epoch: {n + 1}')
         model.fit(tds,
                   validation_data=tds_val,
-                  callbacks=my_callbacks)
+              #    callbacks=my_callbacks
+                  )
+
+main()
