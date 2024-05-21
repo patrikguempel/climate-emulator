ce8dc6baaba9227c2c72ffebf93dbcee8ecf2c28
patrikguempel <patrikguempel@gmail.com>
2024-05-21 23:29:27 +0200

    updated slurm script

--- s-debug.sh
+++ s-debug.sh
@@ -3,7 +3,9 @@
 #SBATCH --time=0:05:00
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
+#SBATCH --output=slurm_output.txt
 
-module load Python
 git pull origin master
+module load TensorFlow
+source myenv/bin/activate
 python main.py remote
