4a0234097493af075a70b3d6c387100fe40b1353
patrikguempel <patrikguempel@gmail.com>
2024-05-22 03:02:50 +0200

    modified slurm script

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=A40short
-#SBATCH --time=1:00:00
+#SBATCH --time=4:00:00
 #SBATCH --gpus=2
 #SBATCH --ntasks=1
-#SBATCH --output=slurm_output.txt
+#SBATCH --output=slurm_output_attempt2.txt
+#SBATCH --job-name=attempt2
 
 module load TensorFlow
 git pull origin master
