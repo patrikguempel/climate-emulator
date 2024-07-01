3031b18ac07514b451986cbf61c1c06f40040800
patrikguempel <patrikguempel@gmail.com>
2024-07-01 17:15:51 +0200

    fixed h5 file

--- evaluate.py
+++ evaluate.py
@@ -1,13 +1,24 @@
 import tensorflow as tf
-print(tf.__version__)
 from tensorflow.python import keras
+from pathlib import Path
 
 
 def evaluate(modelName: str):
-    model: keras.Model = keras.models.load_model(f"./models/{modelName}/model.h5")
+    model: keras.Model = keras.models.load_model(f"./models/own/{modelName}/model.h5")
 
     model.summary()
     # model.evaluate()
 
 
 evaluate("attempt1")
+
+#model_path = Path('models/own/attempt1/model.h5')
+
+# Überprüfen, ob die Datei existiert
+#import h5py
+
+#try:
+#    with h5py.File(model_path, 'r') as f:
+#        print("Die HDF5-Datei ist gültig und kann geöffnet werden.")
+#except Exception as e:
+#    print(f"Fehler beim Öffnen der HDF5-Datei: {e}")
